b903a3e1d56df6f71ec52a0e7418d65a346208bb
HetDesai8 <77717443+HetDesai8@users.noreply.github.com>
2023-06-26 02:27:43 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 # resource "aws_instance" "web_ec2_1" {
-#   ami                         = data.aws_ami.jay-ec2.id
+#   ami                         = data.aws_ami.het-ec2.id
 #   instance_type               = "t2.micro"
 #   subnet_id                   = module.vpc.public_subnets[0]
 #   vpc_security_group_ids      = [module.web_sg.security_group_id]
